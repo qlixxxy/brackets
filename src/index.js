a955baf5beecfff4f215e35c3a65eeea5cab0d3a
qlixxxy <sqlixor@gmail.com>
2020-03-22 22:12:45 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < bracketsConfig.length;) {
+    let a = bracketsConfig[i].join('')
+    if (str.includes(a)) {
+        str = str.replace(a, "")
+        i = 0 
+    } else {
+        i = i + 1
+    } 
+}
+    if (str.length == 0) {
+        return true
+    } else {
+        return false
+  
+    }
 }
